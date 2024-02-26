4699832af3ae39edb38c45a46a18adfbf736ce55
Celine <celine.tandiono21@gmail.com>
2024-02-26 19:38:13 +0000

    sends collected syscall traces to kafka

--- src/monitor.py
+++ src/monitor.py
@@ -2,10 +2,12 @@ from bcc import BPF
 import os
 from datetime import datetime
 import json
+from confluent_kafka import Producer
 
 # Global variables
-requests = 0
 monitor_pid = os.getpid()
+batch_size = 50000
+buffer = 0
 
 def get_path(name):
     relative_path = os.path.join(os.path.dirname(__file__), name)
@@ -44,28 +46,50 @@ def get_ebpf_program(syscalls_mapping):
     """
 
     for i in syscalls_mapping.keys():
-        prog += f"int get_info_{i}(struct pt_regs *ctx) {{\n\
-        return submit_syscall_event(ctx, {syscalls_mapping.get(i)});\n\
-        }}\n\n"
+        prog += f"""
+        int get_info_{i}(struct pt_regs *ctx) {{
+            return submit_syscall_event(ctx, {syscalls_mapping.get(i)});
+        }}"""
     
     return prog
 
 if __name__ == "__main__":
+    #Setup Kafka Producer
+    kafka_config = {'bootstrap.servers': 'localhost:9092'}
+    producer = Producer(kafka_config)
+    topic = 'systemcalls'
+
+    def partitioner(pid):
+        return hash(pid) % 5
+
     with open(get_path("../data/train_syscalls_map.json"), 'r') as file:
         syscalls_mapping = json.load(file)
-
+    
     prog = get_ebpf_program(syscalls_mapping)
-
+   
     #Initialize BPF
     b = BPF(text=prog)
-
-    def print_event(cpu, data, size):
+  
+    def send_to_kafka(cpu, data, size):
         """ Prints details of events triggered """
-        global requests
+        global buffer
+        buffer += 1
         event = b["syscall_events"].event(data)
-        if (event.comm.decode() != "sshd"):
-            requests += 1
-            print(f"PID {event.pid}: | System Call: {event.syscall} | Command:{event.comm.decode()}")
+
+        msg = {}
+        msg["PID"] = event.pid
+        msg["syscall"] = event.syscall
+        msg = json.dumps(msg).encode('utf-8')
+
+        partition = partitioner(event.pid)
+        key = str(event.pid).encode('utf-8')
+        
+        producer.produce(topic_name, value=msg, key=key, partition=partition)
+
+        if buffer == 50000:
+            producer.flush()
+            buffer = 0
+        
 
     # Attach the BPF program to the sys_exit tracepoint (for a specific syscall)
     count = 0
@@ -84,7 +108,7 @@ if __name__ == "__main__":
         file.write('\n'.join(map(str, array)))
 
     # Open the perf buffer
-    b["syscall_events"].open_perf_buffer(print_event)
+    b["syscall_events"].open_perf_buffer(send_to_kafka)
 
     start_time = datetime.now()
     # Poll for events
@@ -93,8 +117,6 @@ if __name__ == "__main__":
             b.perf_buffer_poll()
         except KeyboardInterrupt:
             duration = (datetime.now() - start_time).total_seconds()
-            rate = requests/ duration
-            print(f"{requests} system calls triggered in {duration} seconds")
-            print(f"rate: {rate}")
+            print(f"duration: {duration}")
             print(f"my PID: {monitor_pid}")
             break
