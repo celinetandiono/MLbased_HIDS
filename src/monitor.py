b56ee6d64266dcaa87ab1fb83554c9b7a19b2a76
Celine <celine.tandiono21@gmail.com>
2024-03-03 02:39:19 +0000

    Modified syscall tracer to send kafka in given time window

--- src/monitor.py
+++ src/monitor.py
@@ -1,23 +1,29 @@
+#!/usr/bin/python3
+
 from bcc import BPF
 import os
 from datetime import datetime
 import json
-from confluent_kafka import Producer
+import time
+from kafka import KafkaProducer
 
 # Global variables
 monitor_pid = os.getpid()
-batch_size = 50000
-buffer = 0
+terminal_pid = os.getppid()
+msg_buffer = []
+buffer_timestamp = None
+time_window = 15
 
 def get_path(name):
     relative_path = os.path.join(os.path.dirname(__file__), name)
     absolute_path = os.path.abspath(relative_path)  
     return absolute_path
 
-def get_ebpf_program(syscalls_mapping):
+def get_ebpf_program(syscalls, syscalls_mapping):
     # Define eBPF program
     prog = f"""
     #define MONITOR_PID {monitor_pid}
+    #define TERMINAL_PID {terminal_pid}
     #include <linux/sched.h>
     #include <linux/ptrace.h>
 
@@ -25,15 +31,21 @@ def get_ebpf_program(syscalls_mapping):
         u32 pid;
         u32 syscall;
         char comm[TASK_COMM_LEN];
+        u32 ppid;
     }};
-
+    
     BPF_PERF_OUTPUT(syscall_events);
 
     static int submit_syscall_event(struct pt_regs *ctx, u32 syscall_id) {{
         struct data_t data = {{}};
 
         data.pid = bpf_get_current_pid_tgid() >> 32;
-        if (data.pid == MONITOR_PID){{
+        
+        struct task_struct *task = (struct task_struct *)bpf_get_current_task();
+	data.ppid = task->real_parent->tgid;
+        
+        if (data.pid == MONITOR_PID || data.pid == TERMINAL_PID || data.ppid ==
+        3279 || data.ppid == 1241){{
             return 0;
         }}
 
@@ -45,78 +57,94 @@ def get_ebpf_program(syscalls_mapping):
     }}
     """
 
-    for i in syscalls_mapping.keys():
-        prog += f"""
-        int get_info_{i}(struct pt_regs *ctx) {{
-            return submit_syscall_event(ctx, {syscalls_mapping.get(i)});
-        }}"""
+    for syscall in syscalls:
+        try:
+            syscall_no = syscalls_mapping.get(syscall)
+            prog += f"""
+            int get_info_{syscall}(struct pt_regs *ctx) {{
+                return submit_syscall_event(ctx, {syscall_no});
+            }}"""
+        except:
+            print(f"{syscall}: not found")
     
     return prog
+    
+def send_msg_to_kafka():
+    global msg_buffer
+    print(f"{len(msg_buffer)} messages in buffer")
+    if msg_buffer:
+        for msg in msg_buffer:
+            producer.send('syscalls', value=msg)
+        producer.flush()  # Ensure all buffered messages are sent
+        print("messages sent")
+        message_buffer = []  # Clear the buffer after sending
 
 if __name__ == "__main__":
-    #Setup Kafka Producer
-    kafka_config = {'bootstrap.servers': 'localhost:9092'}
-    producer = Producer(kafka_config)
-    topic = 'systemcalls'
-
-    def partitioner(pid):
-        return hash(pid) % 5
-
     with open(get_path("../data/train_syscalls_map.json"), 'r') as file:
         syscalls_mapping = json.load(file)
     
-    prog = get_ebpf_program(syscalls_mapping)
+    with open(get_path("syscalls.txt")) as file:
+        syscalls = file.read().split()
+    
+    producer = KafkaProducer(
+	bootstrap_servers=['localhost:9092'],
+
+	value_serializer=lambda v: json.dumps(v).encode('utf-8'),
+	acks='all',  # Ensures maximum durability
+	retries=5 # Retry a few times in case of send failure
+    )
+
+    prog = get_ebpf_program(syscalls, syscalls_mapping)
    
     #Initialize BPF
     b = BPF(text=prog)
   
-    def send_to_kafka(cpu, data, size):
+    def print_event(cpu, data, size):
         """ Prints details of events triggered """
-        global buffer
-        buffer += 1
+        global msg_buffer
         event = b["syscall_events"].event(data)
-
-        msg = {}
-        msg["PID"] = event.pid
-        msg["syscall"] = event.syscall
-        msg = json.dumps(msg).encode('utf-8')
-
-        partition = partitioner(event.pid)
-        key = str(event.pid).encode('utf-8')
+        msg = {"PID":event.pid, "syscall": event.syscall}
+        msg_buffer.append(msg)
         
-        producer.produce(topic_name, value=msg, key=key, partition=partition)
-
-        if buffer == 50000:
-            producer.flush()
-            buffer = 0
-        
-
     # Attach the BPF program to the sys_exit tracepoint (for a specific syscall)
     count = 0
-    array = []
-    for syscall_name in syscalls_mapping.keys():
+    failed = []
+    for syscall_name in syscalls:
         try:
-            b.attach_kprobe(event=b.get_syscall_fnname(syscall_name), fn_name=f"get_info_{syscall_name}")
+            b.attach_tracepoint(tp=f"syscalls:sys_exit_{syscall_name}", fn_name=f"get_info_{syscall_name}")
         except:
             count += 1
             print(f"Unable to attach kprobe {count}: {syscall_name}")
-            array.append(syscall_name)
+            failed.append(syscall_name)
             continue
-        
-    file_path = get_path("unable.txt")
-    with open(file_path, 'w') as file:
-        file.write('\n'.join(map(str, array)))
+
+    if failed:    
+        file_path = get_path("unable.txt")
+        with open(file_path, 'w') as file:
+            file.write('\n'.join(map(str, array)))
 
     # Open the perf buffer
-    b["syscall_events"].open_perf_buffer(send_to_kafka)
+    b["syscall_events"].open_perf_buffer(print_event)
 
     start_time = datetime.now()
     # Poll for events
     while True:
         try:
+            current_time = time.time()
             b.perf_buffer_poll()
+            
+            if buffer_timestamp is None:
+                buffer_timestamp = current_time
+            
+            if current_time - buffer_timestamp >= time_window:
+                send_msg_to_kafka() 
+                buffer_timestamp = None
+            
         except KeyboardInterrupt:
+            for syscall_name in syscalls:
+                b.detach_tracepoint(tp=f"syscalls:sys_exit_{syscall_name}")
             duration = (datetime.now() - start_time).total_seconds()
+            print(f"rate: {int(buffer/duration)}")
             print(f"duration: {duration}")
             print(f"my PID: {monitor_pid}")
             break
